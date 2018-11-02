302503eb919ccafc762b277109c5e7a6ce547fba
That JS Dude <khan4019@gmail.com>
2018-11-01 20:59:41 -0500

    dded actions

--- src/components/Photo/Photo.js
+++ src/components/Photo/Photo.js
@@ -1,29 +1,44 @@
 import React, { Component } from 'react';
 
-// Tasks
-// 1. proptypes
+// Learning points
+// 1. prop types
 // 2. default props
-// 3. state from props
+// 3. set state from props
 // 4. toggle css class based on condition
 
 class Photo extends Component {
     constructor(props) {
         super(props);
         this.state={
-            liked:false
+            liked: false,
+            added:false,
         }
+    }
+    
+    toggleLike = () =>{
+        this.setState({liked: !this.state.liked});
+    }
+
+    toggleAddToCart = () => {
+        this.setState({added: !this.state.added})
     }
     
     render() {
         const post = this.props.post;
+        console.log(this.state.liked);
         return (
             <div>
                 <img src={post.url} alt=""/>
                 <div className="icon-container">
-                    <div className="sprite icon heart_empty"></div>
+                    <div 
+                        className={ `sprite icon ' + ${this.state.liked ? 'heart_filled' : 'heart_empty'}`}
+                        onClick={this.toggleLike}
+                    ></div>
                     <div className="sprite icon comment"></div>
-                    <div className="sprite icon add"></div>
-                    <div className="sprite icon remove"></div>
+                    <div 
+                        className={ `sprite icon ' + ${this.state.added ? 'remove' : 'add'}`}
+                        onClick={this.toggleAddToCart}
+                    ></div>
                 </div>
             </div>
         );
